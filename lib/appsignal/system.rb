fe299c4e61b2826cf5c2c506ff7ba5dbf0c8915d
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-02-06 12:38:46 +0100

    Remove Appsignal.System and Appsignal.System.container?

--- lib/appsignal/system.rb
+++ lib/appsignal/system.rb
@@ -1,38 +1,7 @@
 module Appsignal
   module System
-    def self.container?
-      heroku? || Container.id
-    end
-
     def self.heroku?
       ENV.key? "DYNO".freeze
-    end
-
-    module Container
-      CGROUP_FILE = "/proc/self/cgroup".freeze
-
-      class << self
-        def id
-          case cgroups
-          when %r{docker[-|/]([0-9a-f]+)}
-            $1
-          when %r{lxc/([0-9a-f-]+)$} # LXC / Heroku
-            $1
-          end
-        end
-
-        private
-
-        def cgroups
-          file = CGROUP_FILE
-          return unless File.exist? file
-
-          File.read(file)
-        rescue SystemCallError => e
-          Appsignal.logger.debug "Unable to read '#{file}' to determine cgroup"
-          Appsignal.logger.debug e
-        end
-      end
     end
   end
 end
